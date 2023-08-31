64078e04f9f4c59ee6d9f80b9b95d4af17270c07
Timilehin OJO <timilehin175@gmail.com>
2023-08-31 04:58:21 +0100

    Update progress.js

--- progress.js
+++ progress.js
@@ -5,8 +5,15 @@ const fs = require('fs')
 const PORT = 3000
 const HOSTNAME = 'localhost'
 
+const indexPath = path.join(__dirname, 'index.html')
+const errPath = path.join(__dirname, 'err.html')
 
 function requestHandler(req, res){
+if (req.url === '/'){
+    getWeb(req, res)
+}
+
+
 
 }
 
@@ -18,3 +25,13 @@ const server = http.createServer(requestHandler)
 server.listen(PORT, HOSTNAME, () => {
     console.log(`Server Created successfully at http://${HOSTNAME}:${PORT}`)
 })
+
+
+
+
+//Function Handlers
+function getWeb(req, res){
+    res.setheader('content-type', 'text/html')
+    res.writehead(200)
+    res.end(fs.readFileSync(indexPath))
+}
