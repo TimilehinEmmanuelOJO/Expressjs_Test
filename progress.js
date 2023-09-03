676262d4c505830215b0711f3b08e3f7b4b8c22b
Timilehin OJO <timilehin175@gmail.com>
2023-09-03 08:11:10 +0100

    Update progress.js

--- progress.js
+++ progress.js
@@ -12,7 +12,13 @@ function requestHandler(req, res){
 if (req.url === '/'){
     getWeb(req, res)
 }
-
+if(req.url.endsWith('.html') && req.method === 'GET'){
+    try{
+        getRandomWeb(req, res)
+    }catch(err){
+        getErr(req, res)
+    }
+}
 
 
 }
@@ -34,4 +40,19 @@ function getWeb(req, res){
     res.setheader('content-type', 'text/html')
     res.writehead(200)
     res.end(fs.readFileSync(indexPath))
+}
+
+function getRandomWeb(req, res){
+    const file = req.url.split('/')[1]
+    const actualPath = path.join(__dirname, file)
+    const web = fs.readFileSync(actualPath)
+    res.setheader('content-type', 'text.html')
+    res.writehead(200)
+    res.end(web)
+}
+
+function getErr(req, res){
+    res.setheader('content-type', 'text/html')
+    res.writehead(404)
+    res.end(fs.readFileSync(errPath))
 }
