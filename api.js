e515fe4efe804d01a24829cfbbfe4373428dc7a2
Timilehin OJO <timilehin175@gmail.com>
2023-09-04 04:25:39 +0100

    Update Api.json

--- api.js
+++ api.js
@@ -10,8 +10,12 @@ const filePath = path.join(__dirname, 'items.join')
 
 function requestHandler(req, res){
     if (req.url === '/items' && req.method === 'POST'){
-
+        creatItem(req, res)
     }
+    if (req.url === '/items' && req.method === 'GET'){
+        getAllItems(req, res)
+    }
+    if (req.url.startsWith('/items/') && req.method === 'GET')
 }
 
 
@@ -24,9 +28,39 @@ function creatItem (req, res){
     const stringOB = fs.readFileSync(filePath)
     const turnToObj = JSON.parse(stringOB)
     console.log(turnToObj)
+
+    const body = []
+    req.on('data', (chunk) => {
+        body.push(chunk)
+    })
+    req.on('end', () => {
+        const parsedBody = body.toString()
+        const itemsToPost = JSON.parse(parsedBody)
+
+        turnToObj.push({
+            ...itemsToPost,
+            id: Math.floor(Math.random()*500).toString()
+        })
+
+        fs.writeFile(filePath,JSON.stringify(turnToObj)), (err) => {
+            if(err){
+                serverErr()
+            }
+            res.end(JSON.stringify(itemsToPost))
+        }
+    })
+
 }
 
-
+//Get all items
+function getAllItems(req, res){
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err){
+            serverErr()
+        }
+        res.end(data)
+    })
+}
 
 
 
