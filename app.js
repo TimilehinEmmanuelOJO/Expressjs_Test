91b9128508d5560fafb732c2a628edb9d1074c04
Timilehin OJO <timilehin175@gmail.com>
2023-09-06 04:25:48 +0100

    Update App.js

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-const express = require("express");
+const express = require('express')
 
+
+const app = express;
 const PORT = 3000
 
-const app = express()
-
-app.get("/", (req, res) => {
-    res.status(200)
-    res.send("Hello World")
+//creating middleware
+app.use((req, res, next)=>{
+    console.log(`${req.method}${req.path}`)
+    next();
 })
+
+app.get('/',(req, res) => {
+    res.status(200)
+    res.send("timi Ojo")
+})
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
-    console.log(`Server started at  Http://localhost:${PORT}`)
+    console.log(`Server started at port ${PORT}`)
 })
-
