b1589a957573b444f1353e9646cadaf3ae109e63
Timilehin OJO <timilehin175@gmail.com>
2023-09-13 05:02:43 +0100

    Update express routing

--- express_routing/app.js
+++ express_routing/app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const booksRoute = require('./routes/books');
-const authorRoute = require('./routes/authors');
 
-const PORT = 3000;
+const booksRoute = require('./routes/books');
+//const authorRoute = require('./routes/authors');
+
+const PORT = 4000;
 const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.use('/books', booksRoute);
-app.use('/authors', authorRoute);
+//app.use('/authors', authorRoute);
 
 
 app.get('/', (req, res) => {
--- express_routing/book.js
+++ express_routing/book.js
@@ -2,34 +2,52 @@ const express = require('express')
 
 const bookRouter = express.Router()
 
-const books =[
+const books = [
     {
-        title: 'Understanding Node.js',
+        title: 'War and Peace',
         id: 1,
-        year: 2003
+        year: 1869,
     },
     {
-        title: 'Autobiography of Timi',
+        title: 'The Great Gatsby',
         id: 2,
-        year: 2023
+        year: 1925,
     },
     {
-        title: 'The life of Man',
+        title: 'The Catcher in the Rye',
         id: 3,
-        year: 2013
-    },
-    {
-        title: 'Aderonke',
-        id: 4,
-        year: 2012
+        year: 1951,
     },
 ]
 
-bookRouter.get('/',(req, res) => {
+//when you go to home, return all books
+
+bookRouter.get('/', (req, res) => {
     res.json(books)
 })
 
+//when you go to home/id, return the book with the id
+bookRouter.get('/:id',(req, res) => {
+    const id = req.params.id
+    //finds a book in the book array that the id matches what was passed into it
+    const book = books.find(book => book.id == id)
+
+    if (!book){
+        res.status(404)
+        res.end("book not found")
+
+        res.json(book)
+    }
+})
+
+//sending a book into the book array
+bookRouter.post('/', (req, res) => {
+    const body = req.body
+    books.push(book)
+    res.json(book)
+})
 
 
+ module.exports = bookRouter
 
-module.exports = bookRouter
+
